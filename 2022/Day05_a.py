d3dddd2e4eb7d79ce74734e0042ff6c54a1d107f
Lee Clayberg <lee.clayberg@gmail.com>
2022-12-05 00:15:56 -0500

    Day 05

---
+++ 2022/Day05_a.py
@@ -0,0 +1,25 @@
+stacks = [
+['R', 'G', 'J', 'B', 'T', 'V', 'Z'],
+['J', 'R', 'V', 'L'],
+['S', 'Q', 'F'],
+['Z', 'H', 'N', 'L', 'F', 'V', 'Q', 'G'],
+['R', 'Q', 'T', 'J', 'C', 'S', 'M', 'W'],
+['S', 'W', 'T', 'C', 'H', 'F'],
+['D', 'Z', 'C', 'V', 'F', 'N', 'J'],
+['L', 'G', 'Z', 'D', 'W', 'R', 'F', 'Q'],
+['J', 'B', 'W', 'V', 'P'],
+]
+# Read in stuff
+current_total = 0
+with open("files/input_05.txt", "r") as file_stream:
+    while True:
+        line = file_stream.readline()
+        if not line:
+            break
+        move, count, from_, start, to, end = line.split(" ")
+        c, s, e = int(count), int(start), int(end)
+        for i in range(c):
+            box = stacks[s-1].pop()
+            stacks[e-1].append(box)
+
+    print(''.join([sub[-1] for sub in stacks]))
