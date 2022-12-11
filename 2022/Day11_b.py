5676a415196be932e87b781e1f8f013842273de8
Lee Clayberg <lee.clayberg@gmail.com>
2022-12-11 16:32:51 -0500

    Day 11 - No part b

---
+++ 2022/Day11_b.py
@@ -0,0 +1,48 @@
+def create_function(operator, number):
+    if operator == '*':
+        return lambda num: num * (int(number) if number != 'old' else num)
+    else:
+        return lambda num: num + (int(number) if number != 'old' else num)
+
+
+monkeys = dict()
+with open("files/input_11.txt", "r") as file_stream:
+    while True:
+        label = file_stream.readline()
+        if not label:
+            break
+        monkey_num = int(label[:-2].split(' ')[-1])
+        items_line = file_stream.readline()
+        items = [int(i) for i in items_line[:-1].split(': ')[1].split(',')]
+        func_line = file_stream.readline()[:-1].split(' ')
+        func = create_function(func_line[-2], func_line[-1])
+        test = int(file_stream.readline()[:-1].split(' ')[-1])
+        true_case = int(file_stream.readline()[:-1].split(' ')[-1])
+        false_case = int(file_stream.readline()[:-1].split(' ')[-1])
+        file_stream.readline()
+        monkeys[monkey_num] = {
+            'items': items,
+            'operation': func,
+            'test': test,
+            'true': true_case,
+            'false': false_case,
+        }
+
+active = dict()
+for r in range(1000):
+    for key in monkeys.keys():
+        value = monkeys[key]
+        if key not in active.keys():
+            active[key] = 0
+        items_count = len(value['items'])
+        active[key] += items_count
+
+        while len(value['items']) > 0:
+            item = value['items'].pop(0)
+            worry_level = value['operation'](item)
+            thrown_to = value['true'] if worry_level % value['test'] == 0 else value['false']
+            monkeys[thrown_to]['items'].append(worry_level)
+
+print(active)
+num1, num2 = sorted(active.values())[-2:]
+print(num1 * num2)
