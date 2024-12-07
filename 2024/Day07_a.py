38084860e1c1cbfd52a544a1c17598e145881d17
Lee Clayberg <lee.clayberg@gmail.com>
2024-12-07 00:41:03 -0500

    2024 Day 7

---
+++ 2024/Day07_a.py
@@ -0,0 +1,16 @@
+
+# Read in stuff
+with open("files/day07.txt", "r") as file_stream:
+    lines = file_stream.readlines()
+    lines = [line[:-1] for line in lines]
+    equations = [(int(line.split(': ')[0]), [int(n) for n in line.split(': ')[1].split(' ')]) for line in lines]
+
+total = 0
+for cal, nums in equations:
+    answers = {nums[0]}
+    for num in nums[1:]:
+        answers = {answer + num for answer in answers}\
+            .union({answer * num for answer in answers})
+    if cal in answers:
+        total += cal
+print(total)
