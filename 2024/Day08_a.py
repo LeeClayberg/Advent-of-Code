344beab68c2a735213763368ac040c1366ed151f
Lee Clayberg <lee.clayberg@gmail.com>
2024-12-08 01:44:05 -0500

    2024 Day 8

---
+++ 2024/Day08_a.py
@@ -0,0 +1,35 @@
+
+# Read in stuff
+with open("files/day08.txt", "r") as file_stream:
+    lines = file_stream.readlines()
+    lines = [line[:-1] for line in lines]
+
+    antennas = {}
+    height, wide = len(lines), len(lines[0])
+    for y, row in enumerate(lines):
+        for x, cell in enumerate(row):
+            if cell == '.':
+                continue
+            if cell not in antennas:
+                antennas[cell] = []
+            antennas[cell].append((x, y))
+
+    antinodes = set()
+    for positions in antennas.values():
+        length = len(positions)
+        for a in range(0, length):
+            for b in range(0, length):
+                if a == b:
+                    continue
+                ant1, ant2 = positions[a], positions[b]
+                slope = (ant1[0] - ant2[0], ant1[1] - ant2[1])
+                antinode1 = (ant1[0] + slope[0], ant1[1] + slope[1])
+                if 0 <= antinode1[0] < wide and 0 <= antinode1[1] < height:
+                    antinodes.add(antinode1)
+                antinode2 = (ant2[0] - slope[0], ant2[1] - slope[1])
+                if 0 <= antinode2[0] < wide and 0 <= antinode2[1] < height:
+                    antinodes.add(antinode2)
+
+    print(len(antinodes))
+
+
