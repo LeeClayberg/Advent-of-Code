d4e8afbe4f3ae78855b2a65b7946c1d2da2f1489
Lee Clayberg <lee.clayberg@gmail.com>
2024-12-13 01:15:36 -0500

    2024 Day 13

--- 2024/Day13_a.py
+++ 2024/Day13_a.py
@@ -1,41 +1,28 @@
 
 # Read in stuff
-with open("files/day12.txt", "r") as file_stream:
-    lines = file_stream.readlines()
-    farm = [[c for c in line[:-1]] for line in lines]
+with open("files/day13.txt", "r") as file_stream:
+    data = file_stream.read()[:-1]
+    machines = data.split('\n\n')
 
-    # Calculate regions
-    regions = []
-    seen = set()
-    for y, row in enumerate(farm):
-        for x, plot in enumerate(row):
-            position = (x, y)
-            if position in seen:
-                continue
-            region = set()
-            queue = [position]
-            while len(queue) > 0:
-                cx, cy = queue.pop(0)
-                if (cx, cy) in region:
-                    continue
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    nx, ny = cx + dx, cy + dy
-                    if 0 <= nx < len(farm[0]) and 0 <= ny < len(farm) and farm[ny][nx] == plot:
-                        queue.append((nx, ny))
-                region.add((cx, cy))
-                seen.add((cx, cy))
-            regions.append(region)
-
-    # Find fencing
     total = 0
-    for region in regions:
-        perimeter = 0
-        for x, y in region:
-            sides = 4
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < len(farm[0]) and 0 <= ny < len(farm) and farm[ny][nx] == farm[y][x]:
-                    sides -= 1
-            perimeter += sides
-        total += perimeter * len(region)
+    for machine in machines:
+        b1, b2, p = machine.split('\n')
+        b1x, b1y = [int(n) for n in b1.split(': X+')[-1].split(', Y+')]
+        b2x, b2y = [int(n) for n in b2.split(': X+')[-1].split(', Y+')]
+        px, py = [int(n) for n in p.split(': X=')[-1].split(', Y=')]
+        eq1 = lambda a, b: a * b1x + b * b2x == px
+        eq2 = lambda a, b: a * b1y + b * b2y == py
+
+        lowest_cost = 100000000
+        lowest_combo = None
+        for press_a in range(0, 101):
+            for press_b in range(0, 101):
+                if eq1(press_a, press_b) and eq2(press_a, press_b):
+                    cost = press_a * 3 + press_b
+                    if cost < lowest_cost:
+                        lowest_cost = cost
+                        lowest_combo = (press_a, press_b)
+        if lowest_combo is not None:
+            total += lowest_cost
     print(total)
+
--- 2024/Day13_b.py
+++ 2024/Day13_b.py
@@ -1,3 +1,4 @@
+import sympy as sm
 
 # Read in stuff
 with open("files/day13.txt", "r") as file_stream:
@@ -9,20 +10,14 @@ with open("files/day13.txt", "r") as file_stream:
         b1, b2, p = machine.split('\n')
         b1x, b1y = [int(n) for n in b1.split(': X+')[-1].split(', Y+')]
         b2x, b2y = [int(n) for n in b2.split(': X+')[-1].split(', Y+')]
-        px, py = [int(n) for n in p.split(': X=')[-1].split(', Y=')]
-        eq1 = lambda a, b: a * b1x + b * b2x == px
-        eq2 = lambda a, b: a * b1y + b * b2y == py
+        px, py = [int(n) + 10000000000000 for n in p.split(': X=')[-1].split(', Y=')]
 
-        lowest_cost = 100000000
-        lowest_combo = None
-        for press_a in range(0, 101):
-            for press_b in range(0, 101):
-                if eq1(press_a, press_b) and eq2(press_a, press_b):
-                    cost = press_a * 3 + press_b
-                    if cost < lowest_cost:
-                        lowest_cost = cost
-                        lowest_combo = (press_a, press_b)
-        if lowest_combo is not None:
-            total += lowest_cost
+        a, b = sm.symbols(['a', 'b'])
+        sol = sm.solve([a * b1x + b * b2x - px, a * b1y + b * b2y - py], [a, b])
+        sol_a, sol_b = sol[a], sol[b]
+
+        if int(sol_a) != sol_a or int(sol_b) != sol_b:
+            continue
+        cost = sol_a * 3 + sol_b
+        total += cost
     print(total)
-
